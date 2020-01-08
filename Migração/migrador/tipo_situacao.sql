915e7c7a7d72aa8e12c5730fafb82a9f35857279
Silas Campos <silas_campos_23@hotmail.com>
2020-01-08 11:08:11 -0300

    Create tipo_situacao.sql

---
+++ Migração/migrador/tipo_situacao.sql
@@ -0,0 +1,25 @@
+/*
+-- Query: SELECT * FROM hmprojetos.tipo_situacao
+LIMIT 0, 1000
+
+-- Date: 2020-01-08 11:05
+*/
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (1,'Fazer Medição','FM');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (2,'Fazer Desenho','FD');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (3,'Desenhando','D');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (4,'Revisar','R');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (5,'Fazer Contato Terceiro','FCT');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (6,'Fazer Contato Cliente','FCC');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (7,'Aguardando Resposta Cliente','ARC');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (8,'Aguardando Resposta Terceiros','ART');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (9,'Atividades Internas','AI');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (10,'Montar Processo','MP');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (11,'Aguardando','A');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (12,'Entregar','E');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (13,'Arquivar','A');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (14,'Cobrar Pagamento','CP');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (15,'Acompanhamento de Notificações','AN');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (16,'Acompanhamento de Usucapião','AU');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (17,'Elaborando Processo','EP');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (18,'Acompanhamento de Processos','AP');
+INSERT INTO `` (`id`,`nome`,`sigla`) VALUES (20,'Não Especificado',NULL);
