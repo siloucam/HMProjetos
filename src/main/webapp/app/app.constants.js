9af763ea995fcb2fdab3d042063e94a8f5114706
Silas Campos <silascampos23@gmail.com>
2020-01-09 21:22:22 -0300

    build

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('hmProjetosApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
-        .constant('DEBUG_INFO_ENABLED', true)
-        .constant('BUILD_TIMESTAMP', "")
+        .constant('DEBUG_INFO_ENABLED', false)
+        .constant('BUILD_TIMESTAMP', 1578615231045)
 ;
 })();
