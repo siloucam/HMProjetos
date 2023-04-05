40bf09f023219c2a104392ac51479d03df9722ca
Silas Campos <silascampos23@gmail.com>
2023-04-05 15:13:55 -0300

    atualização

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('hmProjetosApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1581084042287)
+        .constant('BUILD_TIMESTAMP', 1672918731166)
 ;
 })();
